c43467f70d1af71d814d8ab8b00bf66e40876c84
tjahan805 <110358481+tjahan805@users.noreply.github.com>
2022-12-06 23:00:35 -0500

    v2

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Game {
+public class Game {
+    private String word;
+    private StringBuffer guess;
+    private StringBuffer tried = new StringBuffer("");
+
+    public Game(String word){
+        this.word = word;
+    }
+
+    public void Hangman() {
+        guess = new StringBuffer(word);
+        for(int i=0; i<guess.length(); i++) {
+            guess.setCharAt(i, '-');
+        }
+        System.out.println("Make sure only the host sees this word!");
+        System.out.println("Word: " + word);
+        System.out.println();
+    }
+
+    public String getGuess() {
+        return guess.toString();
+    }
+
+    public String getTry() {
+
+        return tried.toString();
+    }
+
+    public int tryLetter(char letter) {
+        if (tried.toString().indexOf(letter) >= 0) {
+            return 0;
+        }
+
+        tried.append(letter);
+
+        if (word.indexOf(letter) >= 0) {
+
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) == letter) {
+                    guess.setCharAt(i, letter);
+                }
+            }
+
+            return 1;
+        }
+
+        return -1;
+    }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,54 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Runner {
+    //word list
+    private static final String[] words = {"reindeer", "mistletoe", "christmas", "festive", "present", "rudolph", "eggnog", "santa", "snowman", "gingerbread", "grinch", "ornament", "sleigh", "tinsel", "frostbite", "snowflake", "blizzard", "snowboard", "bobsleigh", "chocolate", "sugarplum", "fruitcake", "peppermint"};
+    public static void main(String[] args){
+        System.out.println("Welcome to Winter/Christmas Hangman! \nThis is a two player game.\nOne player will see the word, one player will guess.\nHeads up! Every failed guess is a body part. 6 failed guesses and you're out! \nStart!");
+        Scanner input = new Scanner(System.in);
+        String word = words[(int) (Math.random() * words.length)];
+        Game game1 = new Game(word);
+        int count = 0;
+        int failedGuesses = 0;
+        game1.Hangman();
+
+        boolean done = false;
+        while(!done){
+            System.out.println("Tried: " + game1.getTry());
+            System.out.println("Failed guesses: " + failedGuesses);
+            System.out.println(game1.getGuess());
+            System.out.println();
+
+            System.out.print("Enter next letter (or type quit): ");
+            String s = input.next();
+            if(s.equals("quit")){
+                System.exit(1);
+            }
+            if (s.length() != 1) {
+                System.out.println("Invalid input");
+            } else {
+                int result = game1.tryLetter(s.charAt(0));
+                if (result == 0) {
+                    System.out.println("Already tried");
+                } else if(result == -1){
+                    System.out.println("Sorry, not there");
+                    failedGuesses++;
+                } else {
+                    System.out.println("Yes!");
+                }
+            }
+            if(failedGuesses >= 6){
+                System.out.println("You guessed too many times! Your man is dead! Game over!");
+                System.exit(1);
+            }
+
+            count++;
+
+            if (game1.getGuess().indexOf('-') == -1) {
+                System.out.println("You guessed " + word + " in " + count + " tries.");
+                done = true;
+            }
+
+        }
     }
 }
